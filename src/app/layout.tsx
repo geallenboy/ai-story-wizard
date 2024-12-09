7efeae0655b1db0139480983daf47cf602876ad4
gejialun <gejialun88@163.com>
2024-12-09 19:03:36 +0800

    ai story wizard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,18 +12,18 @@ export const metadata: Metadata = {
     "AI 故事魔法师是一款智能儿童故事生成 SaaS 产品，通过尖端的人工智能技术，为孩子和家庭带来独特的故事体验。无论是为孩子定制专属的冒险故事，还是为教育者提供寓教于乐的内容，AI 故事魔法师都能轻松满足需求，点燃孩子的想象力和创造力",
 };
 
-const RootLayout = ({ children }: any) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/logo.svg" />
-      </head>
-      <body className={MyAppFont.className}>
-        <ClerkProvider>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/logo.svg" />
+        </head>
+        <body className={MyAppFont.className}>
           <Provider>{children}</Provider>
-        </ClerkProvider>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 };
 
