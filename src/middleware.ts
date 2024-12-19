4f5f7f36de93767b398a29980128e2c01dd1a9f8
gejialun <gejialun88@163.com>
2024-12-19 08:06:17 +0800

    pro error

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/create-story(.*)'])
 
-export default clerkMiddleware(async (auth: any, req) => {
+export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) auth().protect()
 })
 
